c973e03de81d1171764c717732348fe9827a6314
Mihai Gansari <pktehgm@gmail.com>
2025-12-30 13:28:24 +0200

    spring boot responsebody, view, jsp

---
+++ src/main/java/com/mg/webapp1/hello/HelloController.java
@@ -0,0 +1,43 @@
+package com.mg.webapp1.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    // "say-hello" => "Hello! What are you learning today?"
+
+    //Spring looking for a view
+
+    @RequestMapping("/say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("/say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>Html Page 111</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("Hello Html! What are you learning today?");
+        sb.append("</body>");
+        return sb.toString();
+    }
+
+    //JSP
+    //say-hello-jsp => say-hello.jsp
+    //src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    @RequestMapping("/say-hello-jsp")
+    public String sayHelloJsp() {
+        return "sayHello";
+    }
+
+
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Html Page 213</title>
+    </head>
+    <body>
+    Bla bla bla JSP 213
+    </body>
+</html>
